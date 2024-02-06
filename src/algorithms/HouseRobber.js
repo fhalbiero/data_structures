68678637d56d1223bd5d4da39a3143f71515857d
Fabio Albiero <fabio.albiero@trylynk.com>
2024-02-06 16:38:21 -0700

    fix houserobber function

--- src/algorithms/HouseRobber.js
+++ src/algorithms/HouseRobber.js
@@ -2,11 +2,13 @@
 function houseRobber(houses) {
     if (houses.length < 2) return houses[0];
 
-    const total = [houses[0], Math.max(nums[0], nums[1])];
+    const total = [houses[0], Math.max(houses[0], houses[1])];
     for (let i = 2; i < houses.length; i++) {
         total.push(Math.max(houses[i] + total[i - 2], total[i - 1]));
     }
     return total[total.length - 1];
 }
+
+
 //total = 2, 7, 7, 8, 11, 11, 11, 19
-console.log(houseRobber([2, 7, 3, 1, 4, 2, 1, 8]))
+console.log(houseRobber([2, 7, 3, 1, 4, 2, 1, 8]));
